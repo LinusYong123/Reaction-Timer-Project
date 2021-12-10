0cb2a99526da3559b19f72039a6201acf41a8dfd
Linus Yong <86176685+LinusYong123@users.noreply.github.com>
2021-12-10 23:57:20 +0000

    edited the files

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="block">
+  <div class="block" v-if="showBlock">
     click me
   </div>
 </template>
 
 <script>
   export default {
-    props: ['delay']
+    props: ['delay'],
+    data() {
+      return {
+        showBlock: false
+      }
+    },
+    mounted() {
+      console.log('component mounted')
+      setTimeout(() => {
+        this.showBlock = true
+      }, this.delay)
+    }
+    
   }
 </script>
 <style>

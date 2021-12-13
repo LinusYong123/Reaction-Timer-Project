78343decc43e2ab98b9a04ea90b51e7b6a075077
Linus Yong <86176685+LinusYong123@users.noreply.github.com>
2021-12-13 03:59:42 +0000

    PROJECT COMPLETE!!!!!! YAY!!! :)

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,8 +1,31 @@
 <template>
   <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank">{{rank}}</p>
 </template>
 <script>
   export default {
-    props: ['score']
+    props: ['score'],
+    data() {
+      return {
+        rank: null
+      }
+    },
+    mounted() {
+      if (this.score < 350) {
+        this.rank = '🖐💨Ninja Fingers Very Fast🖐💨'
+      } else if (this.score < 400) {
+        this.rank = '👍Rapid Reflexes Good Job👍'
+      } else {
+        this.rank = '🐌Snail pace... Too slow🐌'
+      }
+    }
   }
 </script>
+
+<style>
+  .rank{
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
